f15d8e35ed5fe2d9f2242c12d5d1471042326b61
Sanket Chitagopkar <sanky@sankys.attlocal.net>
2018-04-07 09:20:14 -0700

    Completed DFS and articulation points

--- data_structs/Graph.h
+++ data_structs/Graph.h
@@ -12,17 +12,30 @@ class Graph {
 private:
     typedef function<void (int)> pv_t;
     typedef function<void (int, int)> pe_t;
-    enum state {undiscovered=0, discovered, processed};
+    enum state_t {undiscovered=0, discovered, processed};
+    enum edge_t  {unclassified=0, tree, forward, cross, back};
+
+    struct Edge {
+        int        neghibor;
+        size_t     weight;
+        edge_t     edge_class;
+
+        Edge(int v, size_t w) :
+            neghibor(v), weight(w), edge_class(unclassified) {}
+    };
+
     struct Vertex {
-        list <pair <int, size_t>>    adj_list;
-        state                        vertex_state;
-        size_t                       degree;
-        int                          parent;
-        int                          dfs_entry;
-        int                          dfs_exit;
+        vector<Edge>        adj_list;
+        state_t             state;
+        size_t              degree;
+        int                 parent;
+        int                 dfs_entry;
+        int                 dfs_exit;
+        int                 dfs_low;
+        int                 dfs_treeout;
 
         Vertex () :
-            degree(0), vertex_state(undiscovered),
+            degree(0), state(undiscovered),
             parent(INT_MAX),
             dfs_entry(0), dfs_exit(0)
         {}
@@ -31,20 +44,37 @@ private:
             Vertex()
         {
             degree++;
-            adj_list.push_back(make_pair(v2, weight));
+            adj_list.push_back(Edge(v2, weight));
         }
 
         virtual ~Vertex () {}
 
         void display_adj_list () const
         {
-            for (auto it = adj_list.begin();
-                    it != adj_list.end(); ++it) {
-                cout << (*it).second << "---->" << (*it).first
-                    << "----";
+            for (auto x : adj_list) {
+                cout << x.neghibor
+                    << "---->";
             }
             cout << "|";
         }
+
+        void classify_edge(int v, edge_t e)
+        {
+            for (Edge &edge: adj_list) {
+                if (edge.neghibor == v)
+                    edge.edge_class = e;
+            }
+        }
+
+        edge_t get_edge_class(int v)
+        {
+            for (Edge &edge: adj_list) {
+                if (edge.neghibor == v)
+                    return edge.edge_class;
+            }
+            return unclassified;
+        }
+
 
     };
 
@@ -53,18 +83,12 @@ private:
     size_t                           nvertices;
     bool                             directed;
     size_t                           dfs_time;
-    pv_t                             pv_early;
-    pv_t                             pv_late;
-    pe_t                             pedge;
 
 public:
     Graph(bool dir) :
         nedges(0), nvertices(0),
-        directed(dir), dfs_time(0),
-        pv_early([](int v) {}),
-        pv_late([](int v) {}),
-        pedge([](int u, int v){})
-    {}
+        directed(dir), dfs_time(0) {}
+
     virtual ~Graph() {}
 
     void add_edge (const int v1, const int v2,
@@ -84,37 +108,33 @@ public:
         }
     }
 
-    void set_process_functions (pv_t f1, pv_t f2, pe_t f3)
-    {
-        pv_early = f1,
-        pv_late  = f2;
-        pedge    = f3;
-    }
 
-    void bfs (int key)
+    void bfs (int key, pv_t pv_early, pv_t pv_late,
+              pe_t pe_early, pe_t pe_late)
     {
 
         if (gmap.find(key) == gmap.end())
             return;
 
         __init_graph();
-        __bfs(key, pv_early, pv_late, pedge);
+        __bfs(key, pv_early, pv_late, pe_early, pe_late);
     }
 
-    void dfs (int key)
+    void dfs (int key, pv_t pv_early, pv_t pv_late,
+              pe_t pe_early, pe_t pe_late)
     {
         if (gmap.find(key) == gmap.end())
             return;
 
         __init_graph();
-        __dfs(key, pv_early, pv_late, pedge);
+        __dfs(key, pv_early, pv_late, pe_early, pe_late);
     }
 
     void find_path (int u, int v)
     {
         __init_graph();
         __bfs(u, [](int u){}, [](int u){},
-            [](int u, int v){});
+            [](int u, int v){}, [](int u, int v){});
         cout << "Path";
         __find_path(u, v);
         cout << endl;
@@ -126,13 +146,42 @@ public:
 
         __init_graph();
         for (auto &x: gmap) {
-            if (x.second.vertex_state == undiscovered) {
+            if (x.second.state == undiscovered) {
                 ++c;
                 __bfs(x.first, [](int u){}, [](int u){},
-                      [](int u, int v){});
+                      [](int u, int v){}, [](int u, int v){});
             }
         }
         return c;
+    }
+
+    void find_articulation_points()
+    {
+        __init_graph();
+
+        for (auto &x: gmap) {
+            if (x.second.state == undiscovered) {
+                __dfs(x.first, [](int u){}, [](int u){},
+                      [](int u, int v){}, [](int u, int v){});
+            }
+        }
+        cout << "Articulation point are\t";
+
+        for (auto &x: gmap) {
+            if (x.second.parent == INT_MAX) {
+                if (x.second.dfs_treeout > 1) {
+                    cout << x.first << "\t";
+                }
+            } else {
+                for (auto e : x.second.adj_list) {
+                    if (gmap[e.neghibor].dfs_low >= x.second.dfs_entry) {
+                        cout << x.first << "\t";
+                        break;
+                    }
+                }
+            }
+        }
+        cout << endl;
     }
 
 private:
@@ -140,9 +189,11 @@ private:
     {
         for (auto &x: gmap) {
             x.second.parent = INT_MAX;
-            x.second.vertex_state = undiscovered;
+            x.second.state = undiscovered;
             x.second.dfs_entry = 0;
             x.second.dfs_exit = 0;
+            x.second.dfs_low = INT_MAX;
+            x.second.dfs_treeout = 0;
             dfs_time = 0;
         }
     }
@@ -154,7 +205,7 @@ private:
             gmap[v1] = Vertex(v2, w);
             nvertices++;
         } else {
-            gmap[v1].adj_list.push_back(make_pair(v2, w));
+            gmap[v1].adj_list.push_back(Edge(v2, w));
             gmap[v1].degree++;
         }
 
@@ -165,51 +216,85 @@ private:
         }
     }
 
-    void __bfs (int key, pv_t process_vertex_early,
+    void __bfs (int key,
+                pv_t process_vertex_early,
                 pv_t process_vertex_late,
-                pe_t process_edge)
+                pe_t process_edge_early,
+                pe_t process_edge_late)
     {
         queue<int> q;
         q.push(key);
-        gmap[key].vertex_state = discovered;
+        gmap[key].state = discovered;
         gmap[key].parent = INT_MAX; // assuming INT_MAX never used in graph
         while(!q.empty()) {
             int u = q.front();
             process_vertex_early(u);
-            for (auto it = gmap[u].adj_list.begin();
-                    it != gmap[u].adj_list.end(); ++it) {
-                int v = (*it).first;
-                if (gmap[v].vertex_state == undiscovered) {
-                    gmap[v].vertex_state = discovered;
+            for (auto edge : gmap[u].adj_list) {
+                int v = edge.neghibor;
+                process_edge_early(u, v);
+                if (gmap[v].state == undiscovered) {
+                    gmap[v].state = discovered;
                     gmap[v].parent = u;
-                    process_edge(u, v);
                     q.push(v);
                 }
+                process_edge_late(u, v);
             }
-            gmap[u].vertex_state = processed;
+            gmap[u].state = processed;
             process_vertex_late(u);
             q.pop();
         }
     }
-    void __dfs (int u, pv_t process_vertex_early,
-                pv_t process_vertex_late,
-                pe_t process_edge)
+
+    void __name_dfs_edge(int u, int v)
     {
-        gmap[u].vertex_state = discovered;
-        gmap[u].dfs_entry = dfs_time++;
-        process_vertex_early(u);
-        for (auto it = gmap[u].adj_list.begin();
-             it != gmap[u].adj_list.end(); ++it) {
-            int v = (*it).first;
-            process_edge(u, v);
-            if (gmap[v].vertex_state == undiscovered) {
-                gmap[v].parent = u;
-                __dfs(v, process_vertex_early,
-                      process_vertex_late, process_edge);
+        if (!directed &&
+            gmap[v].get_edge_class(u) != unclassified) {
+            gmap[u].classify_edge(v, gmap[v].get_edge_class(u));
+            return;
+        }
+
+        if (gmap[v].state == undiscovered) {
+            gmap[u].classify_edge(v, tree);
+        } else if (gmap[v].state == discovered) {
+            gmap[u].classify_edge(v, back);
+        } else {
+            if (gmap[u].dfs_entry <
+                gmap[v].dfs_entry) {
+                gmap[u].classify_edge(v, forward);
+            } else {
+                gmap[u].classify_edge(v, cross);
             }
         }
-        gmap[u].dfs_exit = dfs_time++;
-        gmap[u].vertex_state = processed;
+    }
+
+    void __dfs (int u,
+                pv_t process_vertex_early,
+                pv_t process_vertex_late,
+                pe_t process_edge_early,
+                pe_t process_edge_late)
+    {
+        gmap[u].state = discovered;
+        gmap[u].dfs_entry = ++dfs_time;
+        gmap[u].dfs_low  = gmap[u].dfs_entry;
+        process_vertex_early(u);
+        for (auto edge : gmap[u].adj_list) {
+            int v = edge.neghibor;
+            process_edge_early(u, v);
+            __name_dfs_edge(u, v);
+            if (gmap[v].state == undiscovered) {
+                gmap[v].parent = u;
+                __dfs(v, process_vertex_early,
+                      process_vertex_late, process_edge_early,
+                      process_edge_late);
+                gmap[u].dfs_low = min(gmap[u].dfs_low, gmap[v].dfs_low);
+                ++gmap[u].dfs_treeout;
+            } else if (gmap[u].get_edge_class(v) == back) {
+                gmap[u].dfs_low = min(gmap[u].dfs_low, gmap[v].dfs_entry);
+            }
+            process_edge_late(u, v);
+        }
+        gmap[u].dfs_exit = ++dfs_time;
+        gmap[u].state = processed;
         process_vertex_late(u);
     }
 
